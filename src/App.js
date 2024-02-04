795d0a3d170ddd275f4dda013c61cb2cc7f52a51
omerhastaken <omerefesermet@gmail.com>
2024-02-04 11:59:12 +0300

    modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/ChannelDetail.jsx 	new file:   src/components/Feed.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/SearchFeed.jsx 	new file:   src/components/VideoDetail.jsx 	new file:   src/components/index.js 	new file:   src/components/utils/constants.js 	new file:   src/components/utils/fetchFromAPI.js 	modified:   src/index.css 	modified:   src/index.js 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Feed from './components/Feed';
 
 function App() {
   return (
-    <Container>
-      <h1>YouTube!!!</h1>
-    </Container>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Feed />} />
+            <Route path='/video/:id' element={<VideoDetail />}/>
+            <Route path='/channel/:id' element={<ChannelDetail />}/>
+            <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+          </Routes>
+        </Box>
+      </BrowserRouter>
   );
 }
-
-const Container = styled.div``
 
 export default App;
 
